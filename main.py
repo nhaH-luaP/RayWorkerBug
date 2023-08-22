fc6ff1982a20f61739e4923c8e5549b31c7e3175
phahn <hahn-paul@t-online.de>
2023-08-22 13:12:23 +0200

    added alternative script

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import json
 import time
 
 import hydra
@@ -8,10 +7,7 @@ import ray
 import ray.tune as tune
 
 import ray.air as air
-import numpy as np
-
 from omegaconf import OmegaConf
-from ray.tune.search.optuna import OptunaSearch
 
 def train(config):
     time.sleep(5)
@@ -26,6 +22,8 @@ def main(args):
     num_cpus = int(os.environ.get('SLURM_CPUS_PER_TASK', 8))
     num_gpus = torch.cuda.device_count()
     print('>>> Num CPUS:',num_cpus,' Num GPUS:',num_gpus)
+
+    print('>>> Ray IS initialized' if ray.is_initialized() else '>>> Ray IS NOT initialized!')
     
     ray.init(
         num_cpus=num_cpus, 
@@ -33,6 +31,8 @@ def main(args):
         include_dashboard=False,
         _temp_dir=args.output_dir
         )
+    
+    print('''This cluster consists of {} nodes in total {} CPU resources in total and {} GPU resources in total.'''.format(len(ray.nodes()), ray.cluster_resources()['CPU'], ray.cluster_resources()['CPU']))
 
     search_space = {"param": tune.uniform(0, .1)}
     objective = tune.with_resources(train, resources={'cpu': args.cpus_per_trial, 'gpu': args.gpus_per_trial})
@@ -40,7 +40,7 @@ def main(args):
     run_config = air.RunConfig(storage_path=args.output_dir)
     tuner = tune.Tuner(objective, param_space=search_space, tune_config=tune_config, run_config=run_config)
     
-    results = tuner.fit()
+    tuner.fit()
 
     print('>>>>> FINISHED')
 
