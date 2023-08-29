3370e97ffdee3ab488ba4c490361be8b6f73494a
Paul Hahn <phahn@cluster.ies.uni-kassel.de>
2023-08-29 11:11:43 +0200

    Final Commit

--- main.py
+++ main.py
@@ -3,46 +3,39 @@ import json
 import time
 
 import hydra
-import torch
+
 import ray
 import ray.tune as tune
-
 import ray.air as air
-import numpy as np
 
-from omegaconf import OmegaConf
-from ray.tune.search.optuna import OptunaSearch
+import numpy as np
 
 def train(config):
     time.sleep(5)
-    return {"val_metric" : torch.rand((1)).item()}
+    return {"val_metric" : np.random.rand((1)).item()}
 
 
 @hydra.main(version_base=None, config_path=".", config_name="cfg")
 def main(args):
-    print(OmegaConf.to_yaml(args))
     os.makedirs(args.output_dir, exist_ok=True)
-
-    num_cpus = int(os.environ.get('SLURM_CPUS_PER_TASK', 8))
-    num_gpus = torch.cuda.device_count()
-    print('>>> Num CPUS:',num_cpus,' Num GPUS:',num_gpus)
     
+    # Initialize Ray
     ray.init(
-        num_cpus=num_cpus, 
-        num_gpus=num_gpus, 
+        num_cpus=args.num_cpus, 
+        num_gpus=args.num_gpus, 
         include_dashboard=False,
         _temp_dir=args.output_dir
         )
 
+    # Settings for optimization
     search_space = {"param": tune.uniform(0, .1)}
-    objective = tune.with_resources(train, resources={'cpu': args.cpus_per_trial, 'gpu': args.gpus_per_trial})
+    objective = tune.with_resources(train, resources={'cpu': args.num_cpus_per_trial, 'gpu': args.num_gpus_per_trial})
     tune_config = tune.TuneConfig(num_samples=args.num_opt_samples, metric="val_metric", mode="min")
     run_config = air.RunConfig(storage_path=args.output_dir)
     tuner = tune.Tuner(objective, param_space=search_space, tune_config=tune_config, run_config=run_config)
     
+    # Run optimization
     results = tuner.fit()
-
-    print('>>>>> FINISHED')
 
 
 if __name__ == '__main__':
--- slurm_script.sh
+++ slurm_script.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
-#SBATCH --job-name=debug-tests
+#SBATCH --job-name=ray_debug
 #SBATCH --partition=main
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=16
 #SBATCH --gres=gpu:1
 #SBATCH --mem=32GB
-#SBATCH --output=/mnt/stud/work/phahn/dev/logs/%A_%x_%a.out
+#SBATCH --output=/mnt/stud/home/phahn/dev/logs/%A_%x_%a.out
 #SBATCH --array=0-99
 hostname
 
-source activate rayworkerbug
-cd /mnt/stud/work/phahn/dev/RayWorkerBug/
+source ~/envs/rayworkerbug/bin/activate
 
-output_dir=/mnt/stud/work/phahn/dev/output/${SLURM_ARRAY_TASK_ID}_${SLURM_JOB_ID}/
+cd ~/dev/RayWorkerBug/
+
+output_dir=/mnt/stud/home/phahn/dev/output/${SLURM_ARRAY_TASK_ID}_${SLURM_JOB_ID}/
 
 # Run the deep learning script
 srun python -u main.py \
-    output_dir=$output_dir
+    output_dir=$output_dir \
+    num_cpus=16 \
+    num_gpus=1 \
+    num_cpus_per_trial=16 \
+    num_gpus_per_trial=1
